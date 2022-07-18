b4d76e58885e36dbb0aa5388c24a54ebd534e82d
Menescal <lexdmm@protonmail.com>
2022-07-18 16:47:39 -0300

    remove unnecessary files / graphql configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,36 @@
 import { Module } from '@nestjs/common'
-import { GraphQLModule } from '@nestjs/graphql'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { join } from 'path'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { FormsModule } from './forms/forms.module';
+import { FormsModule } from './forms/forms.module'
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot(),
-        GraphQLModule.forRoot({
-            autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+        ConfigModule.forRoot({
+            envFilePath: [`.env`]
         }),
-        FormsModule,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    type: 'postgres',
+                    host: configService.get('DB_HOST'),
+                    port: configService.get('DB_PORT'),
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_NAME'),
+                    synchronize: configService.get('DB_SYNC'),
+                    entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+                    autoLoadEntities: configService.get('ENTITIES_AUTO_LOADS')
+                }
+            }
+        }),
+        FormsModule
     ],
     controllers: [AppController],
-    providers: [AppService]
+    providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
